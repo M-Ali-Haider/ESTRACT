ae8aee95a6763373ceadcdf654e340c72cb0df3d
M-Ali-Haider <136558067+M-Ali-Haider@users.noreply.github.com>
2023-08-10 19:07:29 +0500

    Update headerfooter.js

--- headerfooter.js
+++ headerfooter.js
@@ -2,7 +2,46 @@ document.addEventListener('DOMContentLoaded', function () {
     const burgerMenu = document.querySelector('.burger-menu');
     const menuBar = document.querySelector('.menu-bar');
     const closeButton = document.querySelector('.close-button');
+
     let isOpen = false;
+    let SignContainerOpen=false;
+    let LogContainerOpen=false;
+
+    const signInContainer=document.querySelector('.sign-in-container');
+    const signIn=document.querySelector('.sign-inner');
+    const closeSign=document.querySelector('.close-sign-in');
+
+    const LogInContainer=document.querySelector('.log-in-container');
+    const LogIn=document.querySelector('.log-inner');
+    const closeLog=document.querySelector('.close-log-in');
+
+
+    LogIn.addEventListener('click',()=>{
+        LogInContainer.classList.add('logsign-activator');
+        LogContainerOpen=true;
+        if(SignContainerOpen){
+            SignContainerOpen=false;
+            signInContainer.classList.remove('logsign-activator');
+        }
+    });
+
+    closeLog.addEventListener('click',()=>{
+        LogInContainer.classList.remove('logsign-activator');
+        LogContainerOpen=false;
+    });
+
+    signIn.addEventListener('click',()=>{
+        signInContainer.classList.add('logsign-activator');
+        SignContainerOpen=true;
+        if(LogContainerOpen){
+            LogInContainer.classList.remove('logsign-activator');
+            LogContainerOpen=false;
+        }
+    });
+    closeSign.addEventListener('click',()=>{
+        signInContainer.classList.remove('logsign-activator');
+        SignContainerOpen=false;
+    });
 
     function openMenuBar() {
         isOpen = true;
@@ -14,6 +53,12 @@ document.addEventListener('DOMContentLoaded', function () {
         document.body.classList.remove('menu-open'); 
     }
 
+    function closeLogSignIn(){
+        SignContainerOpen=false;
+        signInContainer.classList.remove('logsign-activator');
+        LogContainerOpen=false;
+        LogInContainer.classList.remove('logsign-activator');
+    }
     burgerMenu.addEventListener('click', function () {
         if (!isOpen) {
             menuBar.classList.add('active');
@@ -29,12 +74,13 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     document.addEventListener('click', function (event) {
-        if (isOpen && !menuBar.contains(event.target) && !burgerMenu.contains(event.target)) {
-            closeMenuBar();
-            menuBar.classList.remove('active');
-        }
+        if (isOpen && !menuBar.contains(event.target) && !burgerMenu.contains(event.target) && !signInContainer.contains(event.target) && !LogInContainer.contains(event.target)) {
+                closeLogSignIn();
+                closeMenuBar();
+                menuBar.classList.remove('active');
+            }
+        });
     });
-});
 
 function updateTotalHeightVariable() {
     const totalHeight = document.body.scrollHeight+20;
